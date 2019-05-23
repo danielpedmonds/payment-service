6aec9f1a350781b6ff0e2b889f9e9871d76d9f61
danielpedmonds <daniel.edmonds.de@googlemail.com>
2019-05-23 22:20:34 +0100

    Implement validation for validating transaction

--- src/main/java/com/revolut/danieledmonds/validation/PaymentValidator.java
+++ src/main/java/com/revolut/danieledmonds/validation/PaymentValidator.java
@@ -1,31 +1,51 @@
 package com.revolut.danieledmonds.validation;
 
-import com.google.gson.Gson;
+import com.revolut.danieledmonds.dao.AccountDao;
+import com.revolut.danieledmonds.domain.Account;
 import com.revolut.danieledmonds.domain.Payment;
 
+import javax.inject.Inject;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentValidator {
 
+    private final AccountDao accountDao;
+
+    @Inject
+    PaymentValidator(final AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
     public void validatePayment(Payment payment) {
 
-        List<String> error = new ArrayList<String>();
-
-        //Validate amount is valid value
         if(!Integer.toString(payment.getAmount()).matches("[0-9]{1,10}")) {
-            error.add("Invalid amount passed");
+            throw new IllegalArgumentException("Field 'amount' should be a positive integer of maximum 10 digits");
         }
 
-        //Get Debiting account
+        if(payment.getDebitingAccountNumber() == null || payment.getDebitingAccountNumber().isEmpty()) {
+            throw new IllegalArgumentException("Field 'debitingAccountNumber' field should be populated");
+        }
 
-        //Validate debiting account has enough funds
+        if(payment.getCreditingAccountNumber() == null ||payment.getCreditingAccountNumber().isEmpty()) {
+            throw new IllegalArgumentException("Field 'creditingAccountNumber' field should be populated");
+        }
 
-        //Validate crediting account exists
+        Account creditingAccount = accountDao.getAccount(payment.getCreditingAccountNumber());
 
+        if (creditingAccount == null) {
+            throw new IllegalArgumentException(String.format("Unable to find 'creditingAccountNumber': %s", payment.getCreditingAccountNumber()));
+        }
 
-        if(!error.isEmpty()) {
-            throw new IllegalArgumentException(new Gson().toJson(error));
+        Account debitingAccount = accountDao.getAccount(payment.getDebitingAccountNumber());
+
+        if (debitingAccount == null) {
+            throw new IllegalArgumentException(String.format("Unable to find 'debitingAccountNumber': %s", payment.getCreditingAccountNumber()));
+        }
+
+        if (payment.getAmount() > debitingAccount.getAccountBalance()){
+            throw new IllegalArgumentException(String.format("Insufficient funds in debiting account '%s'", payment.getDebitingAccountNumber()));
         }
     }
 }
--- src/test/java/com/revolut/danieledmonds/validation/PaymentValidatorTest.java
+++ src/test/java/com/revolut/danieledmonds/validation/PaymentValidatorTest.java
@@ -1,26 +1,156 @@
 package com.revolut.danieledmonds.validation;
 
 import com.google.gson.Gson;
+import com.revolut.danieledmonds.dao.AccountDao;
+import com.revolut.danieledmonds.domain.Account;
 import com.revolut.danieledmonds.domain.Payment;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PaymentValidatorTest {
 
-    PaymentValidator paymentValidator = new PaymentValidator();
+    @Mock
+    AccountDao accountDao;
+
+    private PaymentValidator paymentValidator;
+
+    private Account account() {
+        Account account = new Account();
+        account.setAccountNumber("11111");
+        account.setAccountBalance(10000000);
+        return account;
+    }
+
+    private Payment payment() {
+        Payment payment = new Payment();
+        payment.setAmount(-1000000);
+        payment.setCreditingAccountNumber("11111");
+        payment.setDebitingAccountNumber("22222");
+        return payment;
+    }
 
     @Test
-    public void validateAmount() {
+    public void invalidAmountFails() throws SQLException {
+        paymentValidator = new PaymentValidator(accountDao);
 
-        Payment payment = new Payment();
-        payment.setAmount(-10000000);
-        payment.setCreditingAccountNumber("11111");
-        payment.setDebitingAccountNumber("222222");
+        when(accountDao.getAccount(anyString())).thenReturn(account());
 
-        paymentValidator.validatePayment(payment);
-
+        try {
+            paymentValidator.validatePayment(payment());
+            throw new Exception();
+        } catch (Exception e){
+            assertEquals("Field 'amount' should be a positive integer of maximum 10 digits", e.getMessage());
+        }
     }
+
+    @Test
+    public void emptyDebitAccountNumberFails() throws SQLException {
+        paymentValidator = new PaymentValidator(accountDao);
+
+        when(accountDao.getAccount(anyString())).thenReturn(account());
+
+        Payment payment = payment();
+        payment.setAmount(10000);
+        payment.setCreditingAccountNumber("erer");
+        payment.setDebitingAccountNumber(null);
+
+        try {
+            paymentValidator.validatePayment(payment);
+            throw new Exception();
+        } catch (Exception e){
+            assertEquals("Field 'debitingAccountNumber' field should be populated", e.getMessage());
+        }
+    }
+
+    @Test
+    public void emptyCreditAccountNumberFails() throws SQLException {
+        paymentValidator = new PaymentValidator(accountDao);
+
+        when(accountDao.getAccount(anyString())).thenReturn(account());
+
+        Payment payment = payment();
+        payment.setAmount(10000);
+        payment.setCreditingAccountNumber(null);
+        payment.setDebitingAccountNumber("22222");
+
+        try {
+            paymentValidator.validatePayment(payment);
+            throw new Exception();
+        } catch (Exception e){
+            assertEquals("Field 'creditingAccountNumber' field should be populated", e.getMessage());
+        }
+    }
+
+    @Test
+    public void getCreditingAccountFails() throws SQLException {
+        paymentValidator = new PaymentValidator(accountDao);
+
+        when(accountDao.getAccount(anyString())).thenReturn(null);
+
+        Payment payment = payment();
+        payment.setAmount(10000);
+        payment.setCreditingAccountNumber("11111");
+        payment.setDebitingAccountNumber("22222");
+
+        try {
+            paymentValidator.validatePayment(payment);
+            throw new Exception();
+        } catch (Exception e){
+            assertEquals("Unable to find 'creditingAccountNumber': 11111", e.getMessage());
+        }
+    }
+
+    @Test
+    public void getDebitingAccountFails() throws SQLException {
+        paymentValidator = new PaymentValidator(accountDao);
+
+        when(accountDao.getAccount(anyString())).thenReturn(account()).thenReturn(null);
+
+        Payment payment = payment();
+        payment.setAmount(10000);
+        payment.setCreditingAccountNumber("11111");
+        payment.setDebitingAccountNumber("22222");
+
+        try {
+            paymentValidator.validatePayment(payment);
+            throw new Exception();
+        } catch (Exception e){
+            assertEquals("Unable to find 'debitingAccountNumber': 11111", e.getMessage());
+        }
+    }
+
+    @Test
+    public void insificientFundsCheck() throws SQLException {
+        paymentValidator = new PaymentValidator(accountDao);
+
+        Account account = new Account();
+        account.setAccountNumber("22222");
+        account.setAccountBalance(100);
+
+        when(accountDao.getAccount(anyString())).thenReturn(account);
+        Payment payment = payment();
+        payment.setAmount(10000);
+        payment.setCreditingAccountNumber("11111");
+        payment.setDebitingAccountNumber("22222");
+
+        try {
+            paymentValidator.validatePayment(payment);
+            throw new Exception();
+        } catch (Exception e){
+            assertEquals("Insufficient funds in debiting account '22222'", e.getMessage());
+        }
+    }
+
 }
