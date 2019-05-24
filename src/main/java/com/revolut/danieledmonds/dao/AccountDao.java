5a5399715d8b092d35b032f017d490f1c4a8b852
danielpedmonds <daniel.edmonds.de@googlemail.com>
2019-05-24 10:23:22 +0100

    Changes to database dao

--- src/main/java/com/revolut/danieledmonds/dao/AccountDao.java
+++ src/main/java/com/revolut/danieledmonds/dao/AccountDao.java
@@ -2,6 +2,7 @@ package com.revolut.danieledmonds.dao;
 
 import com.revolut.danieledmonds.api.PaymentApi;
 import com.revolut.danieledmonds.database.Database;
+import com.revolut.danieledmonds.domain.Account;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.sql.PreparedStatement;
@@ -12,16 +13,40 @@ public class AccountDao {
 
     private final static Logger LOG = LoggerFactory.getLogger(PaymentApi.class.getCanonicalName());
 
-    public boolean checkAccountExists(int userId) throws SQLException {
-        LOG.info("About to perform 'checkAccountExists' query");
-        PreparedStatement statement = Database.getStatement("SELECT COUNT(*) FROM ACCOUNTS WHERE ACCOUNT_NUMBER=?");
-        statement.setInt(1, userId);
-        ResultSet count = Database.selectStatement(statement);
-        boolean exists = false;
-        if(count.next()) {
-            exists = count.getInt(1) == 1;
+    public boolean checkAccountExists(String accountNumber) {
+        try {
+            LOG.info("About to perform 'checkAccountExists' query");
+            PreparedStatement statement = Database.getStatement("SELECT COUNT(*) FROM ACCOUNTS WHERE ACCOUNT_NUMBER=?");
+            statement.setString(1, accountNumber);
+            ResultSet count = Database.selectStatement(statement);
+            boolean exists = false;
+            if (count.next()) {
+                exists = count.getInt(1) == 1;
+            }
+            statement.close();
+            return exists;
+        } catch (SQLException e) {
+            LOG.info("Error occurred checking for account with account number: {}", accountNumber);
+            e.printStackTrace();
+            return false;
         }
-        statement.close();
-        return exists;
+    }
+
+    public Account getAccount(String accountNumber) {
+        try {
+            Account account = new Account();
+            PreparedStatement statement = Database.getStatement("SELECT 'ACCOUNT_NUMBER' from ACCOUNTS WHERE ACCOUNT_NUMBER=?");
+            statement.setString(1, accountNumber);
+            ResultSet resultSet = Database.selectStatement(statement);
+            if (resultSet.next()) {
+                account.setAccountNumber(resultSet.getString(1));
+                account.setAccountBalance(resultSet.getInt(2));
+            }
+            return account;
+        } catch (SQLException e) {
+            LOG.info("Error occurred retrieving account with account number: {}", accountNumber);
+            e.printStackTrace();
+            return null;
+        }
     }
 }
