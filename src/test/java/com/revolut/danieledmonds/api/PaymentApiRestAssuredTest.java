cb2e40ff0b95967f4bf02360b9cf2d9fa8370f7d
danielpedmonds <daniel.edmonds.de@googlemail.com>
2019-06-21 08:53:42 +0100

    Amend RestAssured tests based on alternative api responses, added and @AfterClass teardown method to destroy the runtime running of the payment-service.jar that is started for testing.

--- src/test/java/com/revolut/danieledmonds/api/PaymentApiRestAssuredTest.java
+++ src/test/java/com/revolut/danieledmonds/api/PaymentApiRestAssuredTest.java
@@ -1,34 +1,38 @@
 package com.revolut.danieledmonds.api;
 
-import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-
-import com.jayway.restassured.RestAssured;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import java.io.IOException;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class PaymentApiRestAssuredTest {
 
 
     private static final String API_ROOT = "http://localhost:9999";
-
     private static final String API_PAYMENT = "/payment";
+
+    private static Process pr;
 
     @BeforeClass
     public static void setup() throws IOException {
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("java -jar build/libs/payments-service-1.0.jar");
+       pr = rt.exec("java -jar build/libs/payments-service-1.0.jar");
+    }
+
+    @AfterClass
+    public static void teardown() throws  IOException {
+        pr.destroy();
     }
 
     @Test
     public void makeSureThatPaymentFailsWithNoBody() {
         given().when()
                .post(API_ROOT + API_PAYMENT)
-               .then().statusCode(500);
+               .then().statusCode(HttpStatus.BAD_REQUEST_400);
     }
 
     @Test
@@ -37,8 +41,8 @@ public class PaymentApiRestAssuredTest {
                 .body("{ \"debitingAccountNumber\": \"1\", \"creditingAccountNumber\": \"2\", \"amount\": \"-12\"}")
                 .post(API_ROOT + API_PAYMENT)
                 .then()
-                .statusCode(500)
-                .body(equalTo("Transaction failed to be processed: Field 'amount' should be a positive integer of maximum 10 digits"));
+                .statusCode(HttpStatus.BAD_REQUEST_400)
+                .body(equalTo("Invalid arguments passed: Field 'amount' should be a positive integer of maximum 10 digits"));
     }
 
     @Test
@@ -47,8 +51,8 @@ public class PaymentApiRestAssuredTest {
                 .body("{ \"debitingAccountNumber\": \"1\", \"creditingAccountNumber\": \"2\", \"amount\": \"100000\"}")
                 .post(API_ROOT + API_PAYMENT)
                 .then()
-                .statusCode(500)
-                .body(equalTo("Transaction failed to be processed: Insufficient funds in debiting account '1'"));
+                .statusCode(HttpStatus.BAD_REQUEST_400)
+                .body(equalTo("Invalid arguments passed: Insufficient funds in debiting account '1'"));
     }
 
     @Test
@@ -57,7 +61,7 @@ public class PaymentApiRestAssuredTest {
                .body("{ \"debitingAccountNumber\": \"1\", \"creditingAccountNumber\": \"2\", \"amount\": \"12\"}")
                .post(API_ROOT + API_PAYMENT)
                .then()
-               .statusCode(200)
+               .statusCode(HttpStatus.OK_200)
                .body(containsString(" has been inserted"));
     }
 }
